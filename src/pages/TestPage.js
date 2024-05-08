8041a9c659ff96210a0ed0fc6c204f86e5bd6c54
Alireza Entessari <alintssr@gmail.com>
2024-05-08 15:50:16 -0400

    test heroku client

---
+++ src/pages/TestPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TestTable from '../components/TestTable/TestTable'
+export default function TestPage() {
+  return (
+    <div>
+      <h1>This is the Test Page</h1>
+      <TestTable />
+      
+    </div>
+  )
+}
